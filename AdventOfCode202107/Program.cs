15cb112f221cc4cfb75ffb7c4264e0d88337c1de
Fabio Scagliola <scagliola@gmail.com>
2021-12-21 18:52:54 +0100

    Both parts of this puzzle are complete!

---
+++ AdventOfCode202107/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace com.fabioscagliola.AdventOfCode202107
+{
+    class Program
+    {
+        class Result
+        {
+            public int Position { get; protected set; }
+            public int Fuel { get; protected set; }
+
+            public Result(int position, int fuel)
+            {
+                Position = position;
+                Fuel = fuel;
+            }
+
+        }
+
+        static void Main()
+        {
+            List<int> positionList = File.ReadAllText("Input1.txt")
+                .Split(",".ToCharArray(), StringSplitOptions.RemoveEmptyEntries)
+                .ToList().Select(x => int.Parse(x)).ToList();
+
+            // PART 1 
+
+            {
+                List<Result> resultList = new List<Result>();
+
+                for (int i = 0; i < positionList.Max(); i++)
+                {
+                    int fuel = 0;
+
+                    foreach (int position in positionList)
+                    {
+                        fuel += Math.Abs(position - i);
+                    }
+
+                    resultList.Add(new Result(i, fuel));
+                }
+
+                resultList.Sort((a, b) => a.Fuel.CompareTo(b.Fuel));
+
+                Result result = resultList[0];
+
+                Console.WriteLine($"In order to align to position {result.Position} the crabs must spend {result.Fuel} fuel");
+            }
+
+            // PART 2 
+
+            {
+                List<Result> resultList = new List<Result>();
+
+                for (int i = 0; i < positionList.Max(); i++)
+                {
+                    int fuel = 0;
+
+                    foreach (int position in positionList)
+                    {
+                        for (int j = 0; j < Math.Abs(position - i); j++)
+                        {
+                            fuel += j + 1;
+                        }
+                    }
+
+                    resultList.Add(new Result(i, fuel));
+                }
+
+                resultList.Sort((a, b) => a.Fuel.CompareTo(b.Fuel));
+
+                Result result = resultList[0];
+
+                Console.WriteLine($"In order to align to position {result.Position} the crabs must spend {result.Fuel} fuel");
+            }
+        }
+
+    }
+}
+
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# adventofcode.2021.7
-Advent of Code 2021 – Day 7: The Treachery of Whales
+# Advent of Code 2021 – Day 7: The Treachery of Whales
+
+This is how I solved [Advent of Code 2021 – Day 7: The Treachery of Whales](https://adventofcode.com/2021/day/7) 
+
+The solution consists of a console application written in C# based on the Microsoft .NET Framework 4.6 
+
